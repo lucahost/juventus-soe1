07f33134b675c5286528e008d9d66bdb827a8591
Marc Bischof <booooza@gmail.com>
2018-12-02 02:52:50 +0100

    Implement basic command line application menu

--- src/ch/team2/presentation/ConsoleClient.java
+++ src/ch/team2/presentation/ConsoleClient.java
@@ -1,70 +1,74 @@
 package ch.team2.presentation;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import ch.team2.business.person.IPerson;
-import ch.team2.business.person.IPersonFactory;
-import ch.team2.business.person.PersonFactory;
-import ch.team2.persistence.person.IPersonDAO;
-import ch.team2.persistence.person.NaturalPersonMock;
-
 /**
  * Class used to display data
  * Could be replaced due future plans with GUI
  * @author Luca Hostettler
  * @author Marc Bischof
- * @auhor Florian Bohren
+ * @author Florian Bohren
  */
 public class ConsoleClient {
-
-    // attributes
-
-    // constructor
-
-    // methods
 
     /**
      * Initial point
      * @param args
      */
-    public static void main(String[] args) throws IOException {    
-    	try{
-	    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    	System.out.println("Bitte geben Sie den Personentyp ein (natural, legalentity, user)");
-	    	String personenTyp = br.readLine();
-	    	createPersonWithoutScan(personenTyp); 
-	    	//createPersonWithScan();
-        }catch(NumberFormatException nfe){
-            System.err.println("Invalid Format!");
+    public static void main(String[] args) {
+        int mainMenuOption = 0;
+        while (mainMenuOption != 9) {
+            mainMenuOption = ConsoleClientView.getMainMenuSelection();
+            switch(mainMenuOption)
+            {
+                // Kunde anzeigen
+                case 1:
+                    System.out.println("TODO: Kunde anzeigen\n");
+                    break;
+                // Kunde anlegen
+                case 2:
+                    int CreatePersonMenuOption = 0;
+                    while (
+                            CreatePersonMenuOption != 9 &&
+                            CreatePersonMenuOption != 1 &&
+                            CreatePersonMenuOption != 2 &&
+                            CreatePersonMenuOption != 3
+                    ) {
+                        CreatePersonMenuOption = ConsoleClientView.getCreatePersonSelection();
+                        switch(CreatePersonMenuOption)
+                        {
+                            // Natürliche Person
+                            case 1:
+                                ConsoleClientController.createPersonWithoutScan("natural");
+                                break;
+                            // Juristische Person
+                            case 2:
+                                System.out.println("TODO: Juristische Person anlegen\n");
+                                ConsoleClientController.createPersonWithoutScan("legalentity");
+                                break;
+                            // Systembenutzer
+                            case 3:
+                                System.out.println("TODO: Systembenutzer anlegen\n");
+                                ConsoleClientController.createPersonWithoutScan("user");
+                                break;
+                            // Zurück
+                            case 9:
+                                break;
+                            default:
+                                System.out.println("Ungültige Auswahl.\n");
+                                break;
+                        }
+                    }
+                    break;
+                // Beenden
+                case 9:
+                    System.out.println("Kundenverwaltung wird beendet.\n");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Ungültige Auswahl.\n");
+                    break;
+            }
+
         }
     }
-    
-    public static void createPersonWithoutScan(String personenTyp) {
-        System.out.println("start createPerson");
-        
-        //IPersonFactory personFactory = PersonFactory.getInstance();
-        //IPerson person = personFactory.createPerson("Florian","Bohren");
-        IPersonDAO person = null;
-        if(personenTyp.equals("natural")) {
-        	person = new NaturalPersonMock("Florian","Bohren");            
-        } else if(personenTyp.equals("legalentity")) {
-        	//person = new LegalEntityMock("Florian","Bohren");
-        } else if(personenTyp.equals("user")) {
-        	//person = new UserMock("Florian","Bohren");
-        }
-        if(person != null) {
-        	System.out.println("createPerson erfolgreich abgeschlossen. Neue Person: " + person.getDisplayName());
-        } else {
-        	System.out.println("Fehler, keine Person wurde erstellt.");
-        }
-        
-    }
-    
-    public static void createPersonWithScan() {
-    	// get console values
-    }
-    
 }
 
