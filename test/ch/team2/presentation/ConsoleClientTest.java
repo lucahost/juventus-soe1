3774b31bd52847bb8ccdc4535992ab496f2a0144
Marc Bischof <marc.bischof@swisscom.com>
2018-12-16 22:53:52 +0100

    Add functional test for ConsoleClient

--- test/ch/team2/presentation/ConsoleClientTest.java
+++ test/ch/team2/presentation/ConsoleClientTest.java
@@ -1,18 +1,28 @@
 package ch.team2.presentation;
 
-import org.junit.jupiter.api.BeforeEach;
+import ch.team2.business.person.IPersonFactory;
+import ch.team2.business.person.PersonFactory;
+import ch.team2.business.person.PersonType;
+import ch.team2.persistence.person.IPersonDAO;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConsoleClientTest {
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    public void testConsoleClientCanCreatePerson() {
+        IPersonFactory personFactory = PersonFactory.getInstance();
+
+        IPersonDAO testPerson = personFactory.createPerson(
+                PersonType.PERSONTYPE_NATURAL,
+                "Max",
+                "Mustermann"
+        );
+
+        assertEquals(personFactory.displayPerson(0), "Max Mustermann");
+
     }
 
-    @Test
-    void main() {
-    	assertTrue(true);
-    }
+
 }
